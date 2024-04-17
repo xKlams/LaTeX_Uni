1c39b3cd94ce2e19bf8f48de758eaee6f1bf18a1
Federico De Sisti <federicodesisti@gmail.com>
2024-04-17 13:02:10 +0200

    leizone 17

--- Geometria_1/move_pdf.sh
+++ Geometria_1/move_pdf.sh
@@ -2,6 +2,11 @@
 echo "inizio da $1 e finisco a $2"
 for i in $(seq $1 1 $2)
 do
-	cp "$i"_lezione/main.pdf ./PDF_Lezioni/Lezione_$i.pdf
-	echo "Sto copiando la lezione numero $i" 
+	if [$i -le 9]; then
+		cp "$i"_lezione/main.pdf ./PDF_Lezioni/Lezione_$i.pdf
+		echo "Sto copiando la lezione numero $i" 
+	else
+		cp "$i"_lezione/main.pdf ./PDF_Lezioni/Lezione_$i.pdf
+		echo "Sto copiando la lezione numero $i" 
+	fi
 done
