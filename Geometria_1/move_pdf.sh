a4463027f5e31ce8950e69f4e4321e8824fb1e80
Federico De Sisti <federicodesisti@gmail.com>
2024-04-17 23:37:49 +0200

    update 2024-04-17 fino a lezione 18

--- Geometria_1/move_pdf.sh
+++ Geometria_1/move_pdf.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
+date=$(date '+%Y-%m-%d')
 echo "inizio da $1 e finisco a $2"
 for i in $(seq $1 1 $2)
 do
-	if [$i -le 9]; then
-		cp "$i"_lezione/main.pdf ./PDF_Lezioni/Lezione_$i.pdf
+	if [ "$i" -le "9" ]; then
+		cp 0"$i"_lezione/main.pdf ./PDF_Lezioni/0"$i"_Lezione.pdf
 		echo "Sto copiando la lezione numero $i" 
 	else
-		cp "$i"_lezione/main.pdf ./PDF_Lezioni/Lezione_$i.pdf
+		cp "$i"_lezione/main.pdf ./PDF_Lezioni/"$i"_Lezione.pdf
 		echo "Sto copiando la lezione numero $i" 
 	fi
 done
+python3 merge_pdfs.py $1 $2
+git add -A
+git commit -m "update $date fino a lezione $2"
+git push
