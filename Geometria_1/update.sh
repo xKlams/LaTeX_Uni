438b9d97a1538c0552274a08ad57c8d53c4e9313
Federico De Sisti <federicodesisti@gmail.com>
2024-04-18 00:40:48 +0200

    Update 18/04

--- Geometria_1/update.sh
+++ Geometria_1/update.sh
@@ -4,9 +4,13 @@ echo "inizio da $1 e finisco a $2"
 for i in $(seq $1 1 $2)
 do
 	if [ "$i" -le "9" ]; then
+		latexmk -pdf LaTeX_Lezioni/0"$i"_lezione/main.tex
+		latexmk -c
 		cp LaTeX_Lezioni/0"$i"_lezione/main.pdf ./PDF_Lezioni/0"$i"_Lezione.pdf
 		echo "Sto copiando la lezione numero $i" 
 	else
+		latexmk -pdf LaTeX_Lezioni/"$i"_lezione/main.tex
+		latexmk -c
 		cp LaTeX_Lezioni/"$i"_lezione/main.pdf ./PDF_Lezioni/"$i"_Lezione.pdf
 		echo "Sto copiando la lezione numero $i" 
 	fi
