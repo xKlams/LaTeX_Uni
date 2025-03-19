3cfa3f3612eee93e352ebc32ef8ec0863cae391c
Federico De Sisti <federicodesisti@gmail.com>
2025-03-19 14:05:18 +0100

    Update 19/03

--- Analisi_Reale/update.sh
+++ Analisi_Reale/update.sh
@@ -4,10 +4,10 @@ echo "inizio da $1 e finisco a $2"
 for i in $(seq $1 1 $2)
 do
 	if [ "$i" -le "9" ]; then
-		cp LaTeX_Lezioni/0"$i"_lezione/main.pdf ./PDF_Lezioni/0"$i"_Lezione.pdf
+		cp LaTeX_Lezioni/0"$i"_Lezione/main.pdf ./PDF_Lezioni/0"$i"_Lezione.pdf
 		echo "Sto copiando la lezione numero $i" 
 	else
-		cp LaTeX_Lezioni/"$i"_lezione/main.pdf ./PDF_Lezioni/"$i"_Lezione.pdf
+		cp LaTeX_Lezioni/"$i"_Lezione/main.pdf ./PDF_Lezioni/"$i"_Lezione.pdf
 		echo "Sto copiando la lezione numero $i" 
 	fi
 done
