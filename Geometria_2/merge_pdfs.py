c1b7dc075daa55d80320457dafc705d41186274c
Federico De Sisti <federicodesisti@gmail.com>
2025-03-20 08:58:48 +0100

    Update 20/03

--- Geometria_2/merge_pdfs.py
+++ Geometria_2/merge_pdfs.py
@@ -1,9 +1,9 @@
 from pypdf import PdfMerger
 import sys
 
-pdfs = [f"./PDF_Lezioni/0{i}_Lezione.pdf" for i in range(int(sys.argv[1]),10)]
+pdfs = [f"./PDF_Lezioni/Lezione_0{i}.pdf" for i in range(int(sys.argv[1]),10)]
 for i in range(10,int(sys.argv[2]) + 1):
-    pdfs.append(f"./PDF_Lezioni/{i}_Lezione.pdf")
+    pdfs.append(f"./PDF_Lezioni/Lezione_{i}.pdf")
 merger = PdfMerger()
 
 for pdf in pdfs:
