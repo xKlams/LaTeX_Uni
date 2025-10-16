5da5f700360f2b559a5adb5068fd7e12ce294a62
Federico De Sisti <federicodesisti@gmail.com>
2025-10-16 15:23:18 +0200

    16 ottobre

---
+++ Analisi_Numerica/lezione_02/thomas.m
@@ -0,0 +1,9 @@
+f = thomas(a, b, c, f)
+% a = diagonale princiaple (vettore n)
+% b = sottodiagonale inferiroe (vettore n-1)
+% c = sottodiaognale superiore (vettore n-1)
+% vettore soluzione 
+b = [0;b]; % il vettore diventa quindi di n elementi e il secodo elemento si chiamerà b[2], che è quello che voglaimo
+c = [c;0]; % stesso identico motivo di b, ma non ci serve davvero
+
+
